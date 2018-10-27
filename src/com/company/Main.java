5c24e44efb09a906b02550ef195329daf7ae7056
pwnsauce8 <mukanova98@gmail.com>
2018-10-27 20:41:52 +0200

    Update[3]: Edited Floyd-Warshall algorithm

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -10,8 +10,8 @@ public class Main {
     final static int MIN_POCET_SIDEL = 500;
     final static int MAX_POCET_SIDEL = 2000;
 
-    final static int MIN_KM = 1;
-    final static int MAX_KM = 50;
+    final static int MIN_KM = 15;
+    final static int MAX_KM = 100;
 
     final static int MIN_CEST_Z_SIDEL = 200;
     final static int MAX_CEST_Z_SIDEL = 500;
@@ -28,16 +28,28 @@ public class Main {
         Graph graph = new Graph(sidla);
 
         generate_graph(graph, sidla);
-        graph.shortestPath();
-        graph.print_matrix_to_file("/home/janelle/IdeaProjects/logistic_program_sp/src/com/company/output.txt");
+        graph.floyd_warshall();
+        graph.print_matrix_to_file("output.txt");   // vypis grafu (matice)
+        graph.print_matrix_to_file2("output2.txt"); // nejkratsi cesty (matice)
+        System.out.println("Shortest distance from sidlo 1 to 50: " + graph.get_shortest_distance_from_A_to_B(1,50));
 
     }
 
     private static void generate_graph(Graph graph, int sidla) {
         for (int i = 0; i < sidla; i++ ) {
             int prvni_sidlo = i;
+            int cesty =  rand.nextInt((MAX_CEST_Z_SIDEL - MIN_CEST_Z_SIDEL) + 1) + MIN_CEST_Z_SIDEL;
+
+            // Заполнение матрицы бесконечностью
             for (int j = 0; j < sidla; j++ ) {
                 int druhe_sidlo = j;
+                int km = INFINITY;
+                graph.addEgde(prvni_sidlo, druhe_sidlo, km);
+            }
+
+            // Создание от 200-500 дорог
+            for (int j = 0; j < cesty; j++) {
+                int druhe_sidlo = rand.nextInt(sidla);
                 if (prvni_sidlo == druhe_sidlo)
                 {
                     graph.addEgde(prvni_sidlo, druhe_sidlo, INFINITY);
